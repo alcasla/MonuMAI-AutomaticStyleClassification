6e1e376e31a1cd05ca371948df9e930137f58ed2
Alcasla <alcasla90@gmail.com>
2019-05-28 12:32:58 +0200

    trainiing data preparation

--- style_classification.py
+++ style_classification.py
@@ -1,5 +1,6 @@
 import argparse
 import pickle
+from random import randint
 from tensorflow import keras
 
 
@@ -18,15 +19,27 @@ def main():
         metadata = pickle.load(pkl)
         print('Pickle file readed: ', input_file, metadata.shape)
 
-    # prepare the aggregation element vector to be modeled
-    # prepare image categorical label - one-hot encoding
+    # prepare the input data to be modeled
+    train_labels = metadata[:, -1]      # select last column
+    train_data = metadata[:, :metadata.shape[1]-1]      # select all except last column
 
-    # https://medium.com/tebs-lab/how-to-classify-mnist-digits-with-different-neural-network-architectures-39c75a0f03e3
+    # convert the label to one-hot encode
+    train_labels_int = train_labels.astype(int)     # convert class label to int
+    num_classes = max(train_labels_int)+1       # max class index plus one (start from 0)
+    hotone_train_labels = keras.utils.to_categorical(train_labels_int, num_classes)
+
+    print( 'TRAIN data:', train_data.shape )
+    num_sample = randint(0, train_labels.shape[0]-1)
+    print( '\tsample ', train_data[num_sample, :], 'class:', train_labels_int[num_sample], hotone_train_labels[num_sample] )
+
+    ###############################################
 
     # neural networks hyperparameters
     #param
 
     # NN training, validation/test code
+
+    # https://medium.com/tebs-lab/how-to-classify-mnist-digits-with-different-neural-network-architectures-39c75a0f03e3
 
 
 
